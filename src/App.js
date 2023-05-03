c90271ceeafde958046b8e0a40d59dc63d329807
Ole-Magnus Høiback <ole.m.hoiback@gmail.com>
2023-05-03 16:34:57 +0200

    some testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [question, setQuestion] = useState("")
+
+  const handleInput = event  => {
+    setItem(event.target.value)
+  }
+  
+  const handleSubmit = (event) => {
+    const newQuestion = {
+      "question": question
+    }
+
+    /*Her skal det skriver inn post til backend, helst med axios, hvor newQuestion sendes inn!*/
+  }
+
+
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api').then(res => setPeople(res.data));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <form onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label>Question</label>
+        <input
+          type="text"
+          name="question"
+          value={question}
+          onChange={handleInput}
+        />
+      </div>
+      <div className="form-control">
+        <label></label>
+        <button type="submit">Send inn spørsmål</button>
+      </div>
+      <div>
+        {people.map((p, index) => {
+        return <p key={index}>{p.id} {p.name} {p.age}</p>
+        })}
+      </div>
+    </form>
+  </div>
+);
 }
 
 export default App;
