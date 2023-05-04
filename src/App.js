58e9a91f2daead376e38b95b66a8b5512a2d7051
Ole-Magnus Høiback <ole.m.hoiback@gmail.com>
2023-05-04 14:13:47 +0200

    masse nytt

--- src/App.js
+++ src/App.js
@@ -1,52 +1,75 @@
 import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import logo from './nor_logo.png'
+
 
 function App() {
 
   const [question, setQuestion] = useState("")
+  const [answer, setAnswer] = useState("")   
+  const [history, setHistory] = useState([]);
 
+  /*useEffect(() => {
+    axios.get('/api').then(res => setHistory(res.data));
+  }, [answer])*/
+  
   const handleInput = event  => {
-    setItem(event.target.value)
+    setQuestion(event.target.value)
   }
   
   const handleSubmit = (event) => {
-    const newQuestion = {
-      "question": question
+    event.preventDefault();
+    const dataQ = {
+      question: question
     }
 
-    /*Her skal det skriver inn post til backend, helst med axios, hvor newQuestion sendes inn!*/
+    axios.post('/question', dataQ)
+    .then(res => {
+      const newAnswer = res.data;
+      setAnswer(newAnswer); // set the answer based on the server's response
+      setHistory([...history, {question, answer:newAnswer}]);
+    })
+    .catch(err => console.error(err)); // handle any errors
   }
-
-
-  const [people, setPeople] = useState([]);
-
-  useEffect(() => {
-    axios.get('/api').then(res => setPeople(res.data));
-  }, [])
 
   return (
   <div className="App">
-    <form onSubmit={handleSubmit}>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Velkommen til Norconsults egne GPT for personalhåndboka.
+      </p>
+      <form onSubmit={handleSubmit}>
+      <div>Still et spørsmål!</div>
       <div className="form-control">
-        <label>Question</label>
         <input
           type="text"
+          placeholder='Skriv inn spørsmålet ditt her...'
           name="question"
           value={question}
           onChange={handleInput}
+          style={{width: "1000px", height: "80px", backgroundColor: "rgba(0,0,0,0)", color: "white", border: "0px", fontSize: "30px", outline: "none", textAlign: "center"}}
         />
       </div>
-      <div className="form-control">
-        <label></label>
-        <button type="submit">Send inn spørsmål</button>
+      <div className="buttons-container">
+        <button type="submit" class="button-arounder">Send inn spørsmål</button>
       </div>
       <div>
-        {people.map((p, index) => {
-        return <p key={index}>{p.id} {p.name} {p.age}</p>
+        {history.map((p, index) => {
+        return <p key={index}>{p.question} : {p.answer}</p>
         })}
       </div>
     </form>
+    <a
+        className="App-link"
+        href="https://handbooks.simployer.com/nb-no/handbook/100640?sasid=d3e5400f-44fb-4e4d-94db-a65caadf25c1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Personalhåndboka
+      </a>
+    </header>
   </div>
 );
 }
